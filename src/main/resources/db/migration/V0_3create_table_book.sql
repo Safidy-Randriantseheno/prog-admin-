3712e911e2c1ad683ea2bc153c4d19b125665452
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-11-19 00:46:17 +0300

    fix: adding attributes for book

--- src/main/resources/db/migration/V0_3create_table_book.sql
+++ src/main/resources/db/migration/V0_3create_table_book.sql
@@ -4,6 +4,9 @@ $$
         if not exists(select from pg_type where typname = 'topic') then
             create type user_status as enum ('ROMANCE','COMEDY' , 'OTHER' );
         end if;
+        if not exists(select from pg_type where typname = 'availability') then
+                    create type sex as enum ('Disponible', 'Borowed');
+                end if;
     end
 $$;
 create table if not exists "book"
@@ -12,8 +15,9 @@ create table if not exists "book"
         constraint book_pk primary key default uuid_generate_v4(),
     book_name        varchar                  not null,
     page_number        varchar                  not null,
-    sex               sex                      not null,
-    birth_date        date                     not null,
+    release_date        date                     not null,
+    topic               topic                   not null,
+    availability        availability            not null,
     author_id         VARCHAR NOT NULL REFERENCES "author"("id")
 );
 create index if not exists book_name_index on "book" (book_name);
